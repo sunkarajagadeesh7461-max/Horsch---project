2111d9f38b6daf43afa8736092d2520c7a9bc4c2
Jagadeesh <sunkarajagadeesh7461@gmail.com>
2026-07-28 19:05:45 +0200

    Day 1: my first variables

---
+++ Day1.py
@@ -0,0 +1,10 @@
+
+
+name = "Jagadeesh"
+age = 23
+target_company = "Horsch"
+current_German_level = "A2"
+GPA = 2.0
+
+x = f"Hello, my name is {name}, I am {age} years old, and I am currently learning German at level {current_German_level}. My target company is {target_company}, and my GPA is {GPA}."
+print(x)
