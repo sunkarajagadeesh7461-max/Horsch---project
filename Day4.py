4a8e3570d63a4b264b8e8d28ff40f154866762b2
Jagadeesh <sunkarajagadeesh7461@gmail.com>
2026-08-06 15:11:54 +0200

    Day 4: conditional expressions and try/except

--- Day4.py
+++ Day4.py
@@ -1,6 +1,9 @@
+# Conditional Expressions & Error Handling
+
+# field coverage percentage and classifies it as low/medium/high
 field_coverage_percentage = input("Enter the how much percentage of field is covered: ")
 
-try:
+try:               # to handle invalid non-numeric input
     val = float(field_coverage_percentage)
 except ValueError:
     print("Enter a valid number")
@@ -10,7 +13,7 @@ if val < 0:
     print("Enter a positive number")
 elif val <= 40:
     print("The coverage is low")
-elif val >= 40 and val <= 70:
+elif val >= 40 and val <= 70: # chose and instead of or, when we chose or we will never get the output high
     print("The coverage is medium")
 else:
     print("The coverage is High")
