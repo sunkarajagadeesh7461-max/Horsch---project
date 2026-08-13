29027f45b045875eb54a719240b223bd3ad5f0bf
Jagadeesh <sunkarajagadeesh7461@gmail.com>
2026-08-13 13:24:26 +0200

    a changed condition

--- Day4.py
+++ Day4.py
@@ -9,8 +9,8 @@ except ValueError:
     print("Enter a valid number")
     exit()
 
-if val < 0:
-    print("Enter a positive number")
+if val < 0 or val > 100:
+    print("Enter a valid number")
 elif val <= 40:
     print("The coverage is low")
 elif val >= 40 and val <= 70: # chose and instead of or, when we chose or we will never get the output high
